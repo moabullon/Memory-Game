d8c558bda7d8a766ce5830cc26dac9cf211e7c0d
Moa Bullon <moabullon@Moas-MacBook-Pro.local>
2017-01-03 14:06:59 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+  var app = {
+    cards: ['BK.png','BK.png','CFA.jpg','CFA.jpg','Dominos.png','Dominos.png','JM.jpg','JM.jpg','KFC.png','KFC.png','McDonalds.png','McDonalds.png'],
+      init: function() {
+        app.shuffle();
+
+    },
+      shuffle: function() {
+        var random = 0;
+        var temp = 0;
+        for (i = 1; i < app.cards.length; i++){
+          random = Math.round(Math.random() * i);
+          temp = app.cards[i];
+          app.cards[i] = app.cards[random];
+          app.cards[random] = temp;
+        }
+        app.assignCards();
+        console.log(app.cards);
+      },
+      assignCards: function() {
+        $('.card').each(function(index){
+          $(this).attr('data-card-value', app.cards[index]);
+
+        });
+        app.clickHandlers();
+      },
+      clickHandlers: function(){
+        $('.card').on('click', function(){
+          $(this).html('<img src=images/' +$(this).data('cardValue')+'>').addClass('selected');
+          app.checkMatch();
+        });
+    },
+      checkMatch: function() {
+        if($('.selected').length === 2) {
+          if($('.selected').first().data('cardValue') == $('.selected').last().data('cardValue')) {
+            $('.selected').each(function(){
+              $(this).animate({opacity: 0}).removeClass('unmatched');
+            });
+            $('.selected').each(function(){
+              $(this).removeClass('selected');
+              });
+        } else {
+          setTimeout(function(){
+            $('.selected').each(function(){
+              $(this).html('').removeClass('selected');
+            });
+          }, 1000);
+
+         }
+        }
+      },
+  };
+    app.init();
+
+});
